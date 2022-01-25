aa8d4346c993e263515206681f8d7112f5907446
habibouomar <habibou.omar@gmail.com>
2022-01-25 14:50:41 +0100

    first commit

---
+++ jour-1/jour1.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+
+var test = "My name is Omar" 
+console.log(test);
+
+var name = "Omar" ;
+console.log("Nice to meet you "+ name);
+
+var testLength = "I'm very long !"
+console.log(testLength.length);
+
+var food = "croissant is meh"
+food = food.replace("meh", "so good");
+console.log(food);
+
+var basic = "This is Cool"
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic);
+console.log(basicUp);
+console.log(basicDown);
+
+var word = "banana"
+var letters = word.split("")
+console.log(letters);
+
+var age = 32
+var template = "I'm " + age + " years old";
+console.log(template);
